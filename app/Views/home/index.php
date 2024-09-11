c9af1f852bb6fcb76431eaea7d2293c378fe5ff8
MauroRamirezAsus <mauroaramirez88@gmail.com>
2024-09-11 00:35:44 -0300

    Elimino codigo comentado

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -1,32 +1,4 @@
-<!--
-<!DOCTYPE html>
-<html>
-<head>
-    <title> Login - App</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="<?php echo SITE."css/login.css" ?>">
-
-</head>
-<body>
-    <div class="login-container">
-        <form class="login-form">
-            <h2>Iniciar Sesión</h2>
-            <div class="input-group">
-                <label for="email">Correo Electrónico</label>
-                <input type="email" id="email" name="email" placeholder="Ingresar correo" required>
-            </div>
-            <div class="input-group">
-                <label for="password">Contraseña</label>
-                <input type="password" id="password" name="password" placeholder="Password" required>
-            </div>
-            <button type="submit" class="btn-login">Iniciar Sesión</button>
-            <p class="forgot-password"><a href="#">¿Olvidaste tu contraseña?</a></p>
-        </form>
-    </div>
-</body>
-</html>
--->
+<?php echo SITE."css/login.css" ?>">
 <!DOCTYPE html>
 <html>
 <head>
