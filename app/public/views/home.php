e24d38e52b56f7e1861ec0972b978d472e0472f3
MauroRamirezAsus <mauroaramirez88@gmail.com>
2024-10-10 12:24:45 -0300

    Agrego footer en la home

--- app/public/views/home.php
+++ app/public/views/home.php
@@ -18,6 +18,36 @@ if (isset($_SESSION['id'])) {
 			background-color: #cce5ff;
 			/* color celeste */
 		}
+
+		/* Estilos para el footer */
+		footer {
+			color: white;
+			position: absolute;
+			bottom: 0;
+			margin-bottom: 0;
+			width: 100%;
+			/*background-image: url('../img/image.png');*/
+			background: linear-gradient(270deg, #3B4142, #1C474E, black);
+			background-size: 600% 600%;
+            animation: gradientAnimation 20s ease infinite;
+
+			background-position: center;
+			background-repeat: no-repeat;
+			min-height: 15vh;
+			text-align: center;
+			padding: 10px 0;
+		}
+
+		@keyframes gradientAnimation {
+            0% { background-position: 0% 50%; }
+            50% { background-position: 100% 50%; }
+            100% { background-position: 0% 50%; }
+        }
+
+		footer img {
+			height: 50px;
+			/* Ajustar el tamaño del logo */
+		}
 	</style>
 
 	<body class="home-background">
@@ -57,6 +87,14 @@ if (isset($_SESSION['id'])) {
 				</div>
 			</div>
 		</div>
+
+		<footer>
+			<h5>&copy; 2024 - Geolocalización - Practicas Profesionalizantes II</h5>
+			<a href="../views/home.php">
+				<img src="../img/logo.png" alt="Logo de la empresa">
+			</a>
+		</footer>
+
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 	</body>
 
